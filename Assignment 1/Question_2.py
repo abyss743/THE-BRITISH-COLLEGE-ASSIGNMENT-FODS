b17baf344b67d355fe973a2e345748ec373d3f62
abyss reverie <111978670+abyss743@users.noreply.github.com>
2026-04-17 16:08:34 +0545

    Update and rename Question_1.py to Question_2.py

---
+++ Assignment 1/Question_2.py
@@ -0,0 +1,13 @@
+'''
+This program takes two integers and shows addition, multiplication, division, modulus and exponentiation.
+'''
+
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+
+print("\nResults:")
+print("Addition      :", a + b)
+print("Multiplication:", a * b)
+print("Division      :", a / b)
+print("Modulus       :", a % b)
+print("Exponent      :", a ** b)
