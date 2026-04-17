5ff4f7572e7887b9f54b72a08f89ad2c1573738f
abyss reverie <111978670+abyss743@users.noreply.github.com>
2026-04-17 16:10:50 +0545

    Update Question_3.py

--- Assignment 1/Question_3.py
+++ Assignment 1/Question_3.py
@@ -1,1 +1,18 @@
+'''
+This program checks whether the number is positive even, positive odd, negative even or negative odd.
+'''
 
+num = int(input("Enter a number: "))
+
+if num > 0:
+    if num % 2 == 0:
+        print(num, "is Positive Even")
+    else:
+        print(num, "is Positive Odd")
+elif num < 0:
+    if num % 2 == 0:
+        print(num, "is Negative Even")
+    else:
+        print(num, "is Negative Odd")
+else:
+    print("The number is Zero")
